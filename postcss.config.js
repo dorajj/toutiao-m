c27d7a930ebb5df859bc43c58384df0975762bfb
wangjiajia <2116326418@qq.com>
2019-11-16 09:28:41 +0800

    项目起步(vant*rem)

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   plugins: {
-    autoprefixer: {}
+    autoprefixer: {},
+    // 配置计算rem值的插件
+    'postcss-pxtorem': {
+      // 安照37.7的基准值来换算rem单位
+      // vant默认最佳显示状态在ipone6 宽度375px
+      rootValue: 37.5,
+      propList: ['*']
+    }
   }
 }
--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <van-button square type="primary">方形按钮</van-button>
+  </div>
 </template>
 
 <style lang="less">
